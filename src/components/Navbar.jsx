e67b7dfb1fe212ffd75f2419b6cb516ef438767d
Irene Ancilla Chow <ireneancilla@gmail.com>
2025-12-16 10:02:24 +0700

    update: cart on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -91,7 +91,7 @@ const Navbar = () => {
                 <FiShoppingCart className="h-6 w-6" />
                 {getTotalItems() > 0 && (
                   <span className="absolute -top-2 -right-2 bg-red-500 text-white text-[8px] font-bold rounded-full w-5 h-5 flex items-center justify-center">
-                    {getTotalItems() > 100 ? "99+" : getTotalItems()}
+                    {getTotalItems() >= 100 ? "99+" : getTotalItems()}
                   </span>
                 )}
               </button>
